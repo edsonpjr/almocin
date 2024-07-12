39d07b4ea3865cdf36c146d3be8506bf5299f7f3
jdaniielc <jdsc2@cin.ufpe.br>
2024-07-12 15:18:02 -0300

    feat: Add adminPage component and create routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import CreateTest from "./app/home/pages/CreateTest";
 import ListTests from "./app/home/pages/ListTests";
+import CategoryPage from "./app/admin/pages/categoryPage";
+import UserPage from "./app/admin/pages/userPage";
+import MenuPage from "./app/admin/pages/menuPage";
+import AdminPage from "./app/admin/pages/adminPage";
 
 const router = createBrowserRouter([
   {
@@ -15,6 +19,27 @@ const router = createBrowserRouter([
     path: "/tests",
     Component: ListTests,
   },
+  {
+    path: '/adm',
+    children: [
+      {
+        path: '',
+        Component: AdminPage
+      },
+      {
+        path: 'cardapio',
+        Component: MenuPage
+      },
+      {
+        path: 'usuarios',
+        Component: UserPage
+      },
+      {
+        path: 'categorias',
+        Component: CategoryPage
+      }
+    ]
+  }
 ]);
 
 export default function App() {
