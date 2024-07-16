6f4b4644629884d775fb57c241ec85fbf0122d43
Edson Júnior <89272697+edsonpjr@users.noreply.github.com>
2024-07-16 15:30:48 -0300

    Update components-props.ts

--- frontend/src/app/admin/types/components-props.ts
+++ frontend/src/app/admin/types/components-props.ts
@@ -10,28 +10,3 @@ export interface BaseLayoutProps {
   titlePage: string;
   children: React.ReactNode;
 }
-
-export enum StatsFilter {
-  ALL = 'all',
-  MONTH = 'month',
-  MONEY = 'money'
-}
-
-export interface StatsState {
-  filter: StatsFilter;
-  totalRevenue?: number;
-  currentMonthRevenue?: number;
-  totalOrders?: number;
-  monthOrders?: number;
-  averageTicket?: number;
-  currentMonthAverageTicket?: number;
-  totalUsers?: number;
-  totalItems?: number;
-  loading?: boolean;
-  error?: boolean;
-}
-
-export interface StatsAction {
-  type: string;
-  payload?: Partial<StatsState>;
-}
