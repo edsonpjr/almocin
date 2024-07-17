cacaa796bd5808bf2047b22220ac1448c75f22f9
Edson Júnior <89272697+edsonpjr@users.noreply.github.com>
2024-07-17 01:32:02 -0300

    Update reducer.ts

--- frontend/src/app/admin/context/statsContext/reducer.ts
+++ frontend/src/app/admin/context/statsContext/reducer.ts
@@ -1,4 +1,4 @@
-import { StatsState, StatsAction, StatsFilter } from "./types";
+import { StatsState, StatsAction, StatsFilter } from './types';
 
 export const initialState: StatsState = {
   filter: StatsFilter.ALL,
@@ -6,15 +6,10 @@ export const initialState: StatsState = {
 
 export const statsReducer = (state: StatsState, action: StatsAction): StatsState => {
   switch (action.type) {
-    case 'SET_STATS':
+    case 'GET_STATS':
       return {
         ...state,
         ...action.payload,
-      };
-    case 'SET_FILTER':
-      return {
-        ...state,
-        filter: action.payload?.filter ?? StatsFilter.ALL,
       };
     default:
       return state;
