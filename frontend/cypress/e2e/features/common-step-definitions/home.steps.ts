639c756c841f5776fc23525d28a5fef1d97ddef5
Edson Júnior <89272697+edsonpjr@users.noreply.github.com>
2024-07-29 00:23:15 -0300

    Create home.steps.ts

---
+++ frontend/cypress/e2e/features/common-step-definitions/home.steps.ts
@@ -0,0 +1,24 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the tests
+    return false
+})
+
+
+describe('Home flows', () =>{
+    it('acesso da página Cardapio', () => {
+        cy.visit('/adm/cardapio')
+    });
+
+    it('acesso da página de Usuarios', () => {
+        cy.visit('/adm/usuarios')
+    });
+
+    it('acesso da página de Estatísticas', () => {
+        cy.visit('/adm/estatisticas')
+    });
+
+    it('acesso da página de Categorias', () => {
+        cy.visit('/adm/categorias')
+    });
+});
