cb14ababc65fd95e156657d66cc5d0f5c6711bc7
Edson Júnior <89272697+edsonpjr@users.noreply.github.com>
2024-07-29 00:25:16 -0300

    Create stats.steps.ts

---
+++ frontend/cypress/e2e/features/common-step-definitions/stats.steps.ts
@@ -0,0 +1,63 @@
+import { Given, When, Then, Then as And } from "@badeball/cypress-cucumber-preprocessor/";
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the tests
+    return false
+})
+
+beforeEach(() => {
+    cy.visit('/')
+})
+
+Given('o usuário está logado com o email {string} e senha {string}', (admin: string) => {
+    cy.visit("/adm");
+    //cy.get('[data-cy="login"]').type(admin);
+    //cy.get('[data-cy="password"]').type(admin);
+    //cy.get('[data-cy="submit-login"]').click();
+});
+When("acesso a página Estatística", () => {
+    cy.visit("/adm/estatisticas");
+});
+Then("estou na página de título {string}", (title) => {
+    cy.get('h1').should('contain.text', title);
+});
+
+Given("estou na página Estatística", () => {
+    cy.visit("/adm/estatisticas");
+});
+When("seleciono o filtro todos", () => {
+    cy.get('[data-cy="filter-select"]').click();
+    cy.wait(1000);
+    cy.get('[data-cy="filter-todos"]').click();
+    cy.wait(1000);
+});
+Then("posso ver todas as estatíticas", () => {
+    cy.get('[data-cy="filter-select"]').should('contain.text', "Todos");
+});
+
+Given("estou na página Estatística", () => {
+    cy.visit("/adm/estatisticas");
+});
+When("seleciono o filtro mês atual", () => {
+    cy.get('[data-cy="filter-select"]').click();
+    cy.wait(1000);
+    cy.get('[data-cy="filter-mes"]').click();
+    cy.wait(1000);
+});
+Then("posso ver as estatíticas do mês atual", () => {
+    cy.get('[data-cy="filter-select"]').should('contain.text', "Mês Atual");
+});
+
+Given("estou na página Estatística", () => {
+    cy.visit("/adm/estatisticas");
+});
+When("seleciono o filtro mês atual", () => {
+    cy.get('[data-cy="filter-select"]').click();
+    cy.wait(1000);
+    cy.get('[data-cy="filter-receita"]').click();
+    cy.wait(1000);
+});
+Then("posso ver as estatíticas de receita", () => {
+    cy.get('[data-cy="filter-select"]').should('contain.text', "Receita");
+});
